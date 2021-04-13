7ca82a609efb61369eb34f2b3943ec1fb83f8111
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2021-04-13 09:10:36 +0200

    Extract AWS S3 module

--- src/functions/generatePrintFile/handler.ts
+++ src/functions/generatePrintFile/handler.ts
@@ -2,41 +2,13 @@ import "source-map-support/register";
 
 import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
 import { formatJSONResponse } from "@libs/apiGateway";
+import { uploadGratitudesFile } from "@libs/AWS/s3";
 import { middyfy } from "@libs/lambda";
-import { Credentials } from "aws-sdk";
-import * as AWS from "aws-sdk";
-import { filesS3Bucket, region } from "config";
-import { v4 } from "uuid";
 
 import schema from "./schema";
 
 const generatePrintFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-	const s3 = new AWS.S3({
-		region: region,
-		credentials: new Credentials({
-			accessKeyId: process.env.ACCESS_KEY_ID,
-			secretAccessKey: process.env.SECRET_ACCESS_KEY,
-		}),
-	});
-
-	const fileKey = `${v4()}.txt`;
-	await s3
-		.upload({
-			Bucket: filesS3Bucket,
-			Key: fileKey,
-			Body: event.body.gratitudes,
-			ContentType: "text/plain",
-		})
-		.promise();
-
-	const expiry = new Date();
-	expiry.setHours(expiry.getHours() + 1);
-	const downloadUrl = await s3.getSignedUrlPromise("getObject", {
-		Bucket: filesS3Bucket,
-		Key: fileKey,
-		ResponseExpires: expiry,
-	});
-
+	const downloadUrl = await uploadGratitudesFile(event.body.gratitudes);
 	return formatJSONResponse({ downloadUrl });
 };
 
---
+++ src/libs/AWS/s3.ts
@@ -0,0 +1,32 @@
+import { Credentials } from "aws-sdk";
+import S3 from "aws-sdk/clients/s3";
+import { filesS3Bucket, region } from "config";
+import { v4 } from "uuid";
+
+export const uploadGratitudesFile = async (body: S3.Body): Promise<string> => {
+	const s3 = new S3({
+		region: region,
+		credentials: new Credentials({
+			accessKeyId: process.env.ACCESS_KEY_ID,
+			secretAccessKey: process.env.SECRET_ACCESS_KEY,
+		}),
+	});
+
+	const fileKey = `${v4()}.txt`;
+	await s3
+		.upload({
+			Bucket: filesS3Bucket,
+			Key: fileKey,
+			Body: body,
+			ContentType: "text/plain",
+		})
+		.promise();
+
+	const expiry = new Date();
+	expiry.setHours(expiry.getHours() + 1);
+	return await s3.getSignedUrlPromise("getObject", {
+		Bucket: filesS3Bucket,
+		Key: fileKey,
+		ResponseExpires: expiry,
+	});
+};
