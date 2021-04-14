d109304fa2e0a69a7b3db80e2f6ccb5a63cdb859
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2021-04-14 20:28:18 +0200

    Add CORS to generate print file function

--- src/functions/generatePrintFile/index.ts
+++ src/functions/generatePrintFile/index.ts
@@ -17,6 +17,7 @@ export default {
 						"application/json": schema,
 					},
 				},
+				cors: true,
 			},
 		},
 	],
--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,7 +1,8 @@
 import middy from "@middy/core";
+import middyCors from "@middy/http-cors";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const middyfy = (handler) => {
-	return middy(handler).use(middyJsonBodyParser());
+	return middy(handler).use(middyCors()).use(middyJsonBodyParser());
 };
