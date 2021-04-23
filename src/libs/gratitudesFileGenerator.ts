6ac30465aa285137dd2803f13cd101e60760d572
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2021-04-23 09:13:32 +0200

    Set gratitudes cells in gratitudes file dimensions

--- src/libs/gratitudesFileGenerator.ts
+++ src/libs/gratitudesFileGenerator.ts
@@ -1,12 +1,34 @@
-import { Document, ISectionOptions, Packer, Paragraph, Table, TableCell, TableRow, VerticalAlign } from "docx";
+import {
+	Document,
+	HeightRule,
+	ISectionOptions,
+	Packer,
+	Paragraph,
+	Table,
+	TableCell,
+	TableRow,
+	VerticalAlign,
+	WidthType,
+} from "docx";
 
 const ROW_SIZE = 3;
+const A4_DXA_WIDTH = 11906;
+const A4_DXA_HEIGHT = 16838;
+
+const pageStyles = {
+	margin: { top: 0, right: 0, bottom: 0, left: 0 },
+};
 
 export const generateGratitudesFile = async (gratitudesInput: string): Promise<string> => {
 	const gratitudes = gratitudesInput.split("\n");
 
 	const gratitudeCells = gratitudes.map(
-		(g) => new TableCell({ children: [new Paragraph(g)], verticalAlign: VerticalAlign.CENTER }),
+		(g) =>
+			new TableCell({
+				children: [new Paragraph(g)],
+				verticalAlign: VerticalAlign.CENTER,
+				width: { size: A4_DXA_WIDTH / 3, type: WidthType.DXA },
+			}),
 	);
 	const gratitudeRows: Array<TableRow> = [];
 	let rowCells: Array<TableCell> = [];
@@ -14,12 +36,15 @@ export const generateGratitudesFile = async (gratitudesInput: string): Promise<s
 		rowCells.push(gratitudeCells[i]);
 
 		if (i % ROW_SIZE === ROW_SIZE - 1 || i === gratitudeCells.length - 1) {
-			gratitudeRows.push(new TableRow({ children: rowCells }));
+			gratitudeRows.push(
+				new TableRow({ children: rowCells, height: { rule: HeightRule.EXACT, value: A4_DXA_HEIGHT / 10 } }),
+			);
 			rowCells = [];
 		}
 	}
 	const gratitudeTable = new Table({
 		rows: gratitudeRows,
+		columnWidths: [A4_DXA_WIDTH / 3, A4_DXA_WIDTH / 3, A4_DXA_WIDTH / 3],
 	});
 	const gratitudesSection: ISectionOptions = {
 		children: [gratitudeTable],
