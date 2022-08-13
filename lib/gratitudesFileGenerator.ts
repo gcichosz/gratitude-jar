c70496199ea65a83ee6a0ff83cd37e57e589f46e
Grzegorz Cichosz <grzegorz.cichosz@noaignite.com>
2022-08-13 16:32:21 +0200

    feat: create a separate, small page for each card

--- lib/gratitudesFileGenerator.ts
+++ lib/gratitudesFileGenerator.ts
@@ -2,55 +2,41 @@ import { resolve } from "path";
 import PDFDocument from "pdfkit";
 import { PassThrough, Readable } from "stream";
 
-const COLUMNS = process.env.COLUMNS ? parseInt(process.env.COLUMNS, 10) : 3;
-const ROWS = process.env.ROWS ? parseInt(process.env.ROWS, 10) : 10;
 const FONT_SIZE = process.env.FONT_SIZE ? parseInt(process.env.FONT_SIZE, 10) : 16;
-const DRAW_GRID = process.env.DRAW_GRID === "true";
-const LONG_LINE_THRESHOLD = process.env.LONG_LINE_THRESHOLD ? parseInt(process.env.LONG_LINE_THRESHOLD, 10) : 35;
+const CARD_WIDTH_MM = process.env.CARD_WIDTH_MM ? parseInt(process.env.CARD_WIDTH_MM, 10) : 70;
+const CARD_HEIGHT_MM = process.env.CARD_HEIGHT_MM ? parseInt(process.env.CARD_HEIGHT_MM, 10) : 30;
 
-const A4_WIDTH = 595.28;
-const A4_HEIGHT = 841.89;
-const COLUMN_WIDTH = A4_WIDTH / COLUMNS;
-const ROW_WIDTH = A4_HEIGHT / ROWS;
-const TEXT_X_START = 0;
-const TEXT_Y_START = ROW_WIDTH / 2;
-const MAGIC_Y_OFFSET = FONT_SIZE / 2 + 2;
+const postScriptPointCoefficient = 2.83465;
+const cardWidth = CARD_WIDTH_MM * postScriptPointCoefficient;
+const cardHeight = CARD_HEIGHT_MM * postScriptPointCoefficient;
+const margin = 5 * postScriptPointCoefficient;
+const textOptions: PDFKit.Mixins.TextOptions = {
+	align: "center",
+	width: cardWidth - margin * 2,
+};
 
 export const generateGratitudesFile = (gratitudesInput: string): Readable => {
 	const gratitudes = gratitudesInput.split("\n");
 
 	const stream = new PassThrough();
-	const doc = new PDFDocument({ size: "A4", margin: 0 });
+	const doc = new PDFDocument({
+		size: [cardWidth, cardHeight],
+		margin: margin,
+	});
 	doc.pipe(stream);
 	doc.fontSize(FONT_SIZE);
+	doc.font(resolve(__dirname, "fonts/Caveat-Regular.ttf"));
 
-	let textX = TEXT_X_START;
-	let textY = TEXT_Y_START;
 	for (let i = 0; i < gratitudes.length; i++) {
 		const gratitude = gratitudes[i];
-		const offsetY = Math.floor(gratitude.length / LONG_LINE_THRESHOLD) * MAGIC_Y_OFFSET;
+		doc.text(
+			gratitude,
+			doc.x,
+			doc.y + 0.5 * (cardHeight - margin * 2 - doc.heightOfString(gratitude, textOptions)),
+			textOptions,
+		);
 
-		doc.font(resolve(__dirname, "fonts/Caveat-Regular.ttf")).text(gratitude, textX, textY - offsetY, {
-			align: "center",
-			baseline: "middle",
-			width: COLUMN_WIDTH,
-		});
-
-		textX += COLUMN_WIDTH;
-
-		if (i % COLUMNS === COLUMNS - 1) {
-			textX = TEXT_X_START;
-			textY += ROW_WIDTH;
-		}
-
-		const lastGratitudeOnPage = i % (ROWS * COLUMNS) === ROWS * COLUMNS - 1;
-		const lastGratitude = i === gratitudes.length - 1;
-		if (DRAW_GRID && (lastGratitudeOnPage || lastGratitude)) {
-			drawGrid(doc);
-		}
-
-		if (lastGratitudeOnPage && !lastGratitude) {
-			textY = TEXT_Y_START;
+		if (i < gratitudes.length - 1) {
 			doc.addPage();
 		}
 	}
@@ -58,22 +44,4 @@ export const generateGratitudesFile = (gratitudesInput: string): Readable => {
 	doc.end();
 
 	return stream;
-};
-
-const drawGrid = (doc: PDFKit.PDFDocument) => {
-	for (let i = 1; i < COLUMNS; i++) {
-		doc.moveTo(COLUMN_WIDTH * i, 0)
-			.lineTo(COLUMN_WIDTH * i, A4_HEIGHT)
-			.lineWidth(1)
-			.dash(5, { space: 10 })
-			.stroke();
-	}
-
-	for (let i = 1; i < ROWS; i++) {
-		doc.moveTo(0, ROW_WIDTH * i)
-			.lineTo(A4_WIDTH, ROW_WIDTH * i)
-			.lineWidth(1)
-			.dash(5, { space: 10 })
-			.stroke();
-	}
 };
